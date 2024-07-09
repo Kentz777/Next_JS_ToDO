822d7df58368306e40187573fe7b65729b192aa6
Kentz777 <116345480+Kentz777@users.noreply.github.com>
2024-07-09 16:40:39 +0800

    second push

---
+++ app/components/EditTopicForm.jsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+function EditTopicForm({ id, title, description }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form 
+      onSubmit={handleSubmit}
+      className='flex flex-col gap-3'
+    >
+      <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        className='border border-slate-500 py-2 px-8'
+        placeholder='Topic Title'
+        type='text'
+      />
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        className='border border-slate-500 py-2 px-8'
+        placeholder='Topic Description'
+        type='text'
+      />
+      <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit'>
+        Update Topic
+      </button>
+    </form>
+  );
+}
+
+export default EditTopicForm;
